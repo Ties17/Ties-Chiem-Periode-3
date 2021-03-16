bb0f6754d17160a472b981a320b4cde72c17a981
Ties Tienhoven <tiestienhoven@gmail.com>
2021-03-16 12:37:44 +0100

    Time keeping added to smart meter

--- IWSN/Smartmeter Ties Chiem/src/main.cpp
+++ IWSN/Smartmeter Ties Chiem/src/main.cpp
@@ -4,13 +4,18 @@
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
 #include <SmartmeterConfig.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 WiFiClient wifi;
 PubSubClient mqtt;
 SoftwareSerial p1;
 
+WiFiUDP udp;
+NTPClient ntp(udp, "pool.ntp.org", 3600);
+
 DynamicJsonDocument doc(1024);
-char output[];
+char output[1024];
 
 void connectMQTT()
 {
@@ -33,7 +38,6 @@ void connectMQTT()
 void setup() {
   
   // put your setup code here, to run once:
-
   Serial.begin(9600);
 
   WiFi.begin(WIFI_SSID, WIFI_PASS); // Connect to the Wi-Fi (if not known use WifiManager from tzapu!)
@@ -53,8 +57,18 @@ void setup() {
   mqtt.setBufferSize(256); // override MQTT_MAX_PACKET_SIZE
   mqtt.setServer(MQTT_SERVER, MQTT_PORT);
   connectMQTT();
+
+  ntp.begin();
 }
 
 void loop() {
+  ntp.update();
   // put your main code here, to run repeatedly:
+  
+  JsonObject obj = doc.to<JsonObject>();
+  obj["MQTT_USER"] = MQTT_USER;
+  obj["Time"] = ntp.getFormattedTime();
+  serializeJson(obj, output);
+  mqtt.publish(MQTT_TOPIC_DATA, output);
+  delay(10000);
 }
