3a1a695d54853e3b6748af06a91f415ad5f217db
Ties Tienhoven <tiestienhoven@gmail.com>
2021-03-20 12:03:06 +0100

    update ip adress for connection to db

--- IWSN/Smarmeter backend/backend.js
+++ IWSN/Smarmeter backend/backend.js
@@ -2,12 +2,12 @@ var mqtt = require('mqtt')
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 
-const url = 'mongodb://localhost:27017'
+const url = 'mongodb://192.168.0.129:27017'
 const dbName = 'smartmeter'
 const client = new MongoClient(url)
 var mqtt = mqtt.connect('mqtt://test.mosquitto.org')
 
-client.connect(function(err) {
+client.connect(function (err) {
     assert.strictEqual(null, err)
     console.log('Connected successfully to server')
 
@@ -16,7 +16,7 @@ client.connect(function(err) {
     const login = db.collection("login")
     const data = db.collection("data")
     mqtt.on("message", function (topic, message) {
-        try{
+        try {
             var doc = JSON.parse(message)
             if (topic == "SMARTMETER-TIES-CHIEM-DATA") {
                 data.insertOne(doc)
@@ -24,10 +24,10 @@ client.connect(function(err) {
             if (topic == "SMARTMETER-TIES-CHIEM-LOGIN") {
                 login.insertOne(doc)
             }
-        } catch (error){
+        } catch (error) {
             console.error(error)
         }
-        
-        
+
+
     })
 })
