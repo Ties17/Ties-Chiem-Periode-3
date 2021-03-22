243d8e657c4ab7fda43bb61d6e4f1bd604ab4003
ChiemStevens <Chiem.stevens@outlook.com>
2021-03-22 18:01:30 +0100

    working zigbee coordinator

---
+++ IWSN/Smartmet-zigbee-coordinator/src/config.h
@@ -0,0 +1,7 @@
+constexpr char MQTT_SERVER[] = "test.mosquitto.org";
+constexpr int MQTT_PORT = 1883;
+constexpr char* MQTT_TOPIC = "iwsn-wemos-client-chiem";
+constexpr char* MQTT_SUBSCRIBE = "iwsn-wemos-chiem";
+
+constexpr char* WIFI_USERNAME = "H368N";
+constexpr char* WIFI_PASSWORD = "MaElJoCh";
