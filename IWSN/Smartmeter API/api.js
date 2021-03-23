f1744dfad2df7b462656b9ade5abd86890eff0ea
Ties Tienhoven <tiestienhoven@gmail.com>
2021-03-23 14:16:01 +0100

    Update to API to be able to request data by after a surtain time

--- IWSN/Smartmeter API/api.js
+++ IWSN/Smartmeter API/api.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 3000
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
@@ -7,17 +8,20 @@ const url = 'mongodb://192.168.0.129:27017'
 const dbName = 'smartmeter'
 const client = new MongoClient(url)
 
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/allData', (req, res) => {
+//////////////////////////////////////////
+//          SMARTMETER SECTION          //
+//////////////////////////////////////////
+
+app.get('/smartmeter/allData', (req, res) => {
     client.connect(function (err) {
         assert.strictEqual(null, err)
         const db = client.db(dbName)
-        const data = db.collection("data")
-        var query = { Time: { $gt: 1616411511}}
+        const data = db.collection("smartmeterdata")
+        var query = {}
         data.find(query).toArray((function(err, result) {
             if(err) throw err
             res.send(result)
@@ -25,8 +29,47 @@ app.get('/allData', (req, res) => {
     })
 })
 
-app.get('/dataByTime', (req, res) => {
-    
+app.get('/smartmeter/dataByTime', (req, res) => {
+    client.connect(function (err) {
+        assert.strictEqual(null, err)
+        const db = client.db(dbName)
+        const data = db.collection("smartmeterdata")
+        var query = { Time: {$gt: req.body['Time'] }}
+        data.find(query).toArray((function (err, result) {
+            if (err) throw err
+            res.send(result)
+        }))
+    })
+})
+
+//////////////////////////////////////////
+//          SENSORS SECTION             //
+//////////////////////////////////////////
+
+app.get('/sensors/allData', (req, res) => {
+    client.connect(function (err) {
+        assert.strictEqual(null, err)
+        const db = client.db(dbName)
+        const data = db.collection("sensordata")
+        var query = {}
+        data.find(query).toArray((function(err, result) {
+            if(err) throw err
+            res.send(result)
+        }))
+    })
+})
+
+app.get('/sensors/dataByTime', (req, res) => {
+    client.connect(function (err) {
+        assert.strictEqual(null, err)
+        const db = client.db(dbName)
+        const data = db.collection("sensordata")
+        var query = { Time: {$gt: req.body['Time']}}
+        data.find(query).toArray((function(err, result) {
+            if(err) throw err
+            res.send(result)
+        }))
+    })
 })
 
 app.listen(port, () => {
