e837ebd1c8b455fd9dcbdc5bcaa360b32de3e0c5
Ties Tienhoven <tiestienhoven@gmail.com>
2021-03-23 17:04:04 +0100

    update to fix cors

--- IWSN/Smartmeter API/api.js
+++ IWSN/Smartmeter API/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 const port = 3000
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
