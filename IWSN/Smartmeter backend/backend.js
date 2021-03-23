fddc5ebeecff776297bb22547afdcd9bbbfe94e1
Ties Tienhoven <tiestienhoven@gmail.com>
2021-03-23 15:16:04 +0100

    change to all json keys so they have underscores instead of spaces

--- IWSN/Smartmeter backend/backend.js
+++ IWSN/Smartmeter backend/backend.js
@@ -21,7 +21,7 @@ client.connect(function (err) {
             var doc = JSON.parse(message)
             if (topic == "SMARTMETER-TIES-CHIEM-LOGIN") {
                 login.insertOne(doc)
-            }else {
+            } else {
                 if (doc['MQTT_USER'].match('SMARTMETER') != null) {
                     doc = parseDataToJson(doc);
                 }
@@ -33,7 +33,7 @@ client.connect(function (err) {
                         sensordata.insertOne(doc)
                     }
                 }
-            }            
+            }
         } catch (error) {
             console.error(error)
         }
@@ -43,8 +43,8 @@ client.connect(function (err) {
 function parseDataToJson(doc) {
     var json = {}
     var data = doc['Data'].split("\r\n")
-    for(var key in doc){
-        if(key != 'Data'){
+    for (var key in doc) {
+        if (key != 'Data') {
             json[key] = doc[key]
         }
     }
@@ -58,7 +58,7 @@ function parseDataToJson(doc) {
     fdata.forEach(element => {
         var key = element.split('(')[0]
         var d = element.slice(key.length)
-        if(d.indexOf('(') == d.lastIndexOf('(')){
+        if (d.indexOf('(') == d.lastIndexOf('(')) {
             d = d.substring(d.indexOf('(') + 1, d.indexOf(')'))
         } else {
             d = d.split(')(')
@@ -66,7 +66,7 @@ function parseDataToJson(doc) {
             d[d.length - 1] = d[d.length - 1].substring(0, d.length - 1)
         }
         key = obisRefSwitchTable(key)
-        if(key != null){
+        if (key != null) {
             json[key] = d
         }
     })
@@ -77,69 +77,69 @@ function parseDataToJson(doc) {
 function obisRefSwitchTable(code) {
     switch (code) {
         case '1-3:0.2.8':
-            return "P1 output version information"
+            return "P1_output_version_information"
         case '0-0:1.0.0':
-            return "P1 message timestamp"
+            return "P1_message_timestamp"
         case '0-0:96.1.1':
-            return "Equipment id"
+            return "Equipment_id"
         case '1-0:1.8.1':
-            return "electricity delivered to client (tariff 1) in 0,001kWh"
+            return "electricity_delivered_to_client_(tariff_1)_in_0,001kWh"
         case '1-0:1.8.2':
-            return "electricity delivered to client (tariff 2) in 0,001kWh"
+            return "electricity_delivered_to_client_(tariff_2)_in_0,001kWh"
         case '1-0:2.8.1':
-            return "electricity delivered by client (tariff 1) in 0,001kWh"
+            return "electricity_delivered_by_client_(tariff_1)_in_0,001kWh"
         case '1-0:2.8.2':
-            return "electricity delivered by client (tariff 2) in 0,001kWh"
+            return "electricity_delivered_by_client_(tariff_2)_in_0,001kWh"
         case '0-0:96.14.0':
-            return "Tariff indicator elextricity"
+            return "Tariff_indicator_elextricity"
         case '1-0:1.7.0':
-            return "Actual electricity power delivered (+P) in 1 watt resolution"
+            return "Actual_electricity_power_delivered_(+P)_in_1_watt_resolution"
         case '1-0:2.7.0':
-            return "Actual electricity power received (-P) in 1 Watt resolution"
+            return "Actual_electricity_power_received_(-P)_in_1_Watt_resolution"
         case '0-0:96.7.21':
-            return "Number of power failures in any phase"
+            return "Number_of_power_failures_in_any_phase"
         case '0-0:96.7.9':
-            return "Number of long power failures in any phase"
+            return "Number_of_long_power_failures_in_any_phase"
         case '1-0:99.97.0':
-            return "Power Failure Event Log (long power failures)"
+            return "Power_Failure_Event_Log_(long_power_failures)"
         case '1-0:32.32.0':
-            return "Number of voltage sags in phase L1"
+            return "Number_of_voltage_sags_in_phase_L1"
         case '1-0:52.32.0':
-            return "Number of voltage sags in phase L2"
+            return "Number_of_voltage_sags_in_phase_L2"
         case '1-0:72.32.0':
-            return "Number of voltage sags in phase L3"
+            return "Number_of_voltage_sags_in_phase_L3"
         case '1-0:32.36.0':
-            return "Number of voltage swells in phase L1"
+            return "Number_of_voltage_swells_in_phase_L1"
         case '1-0:52.36.0':
-            return "Number of voltage swells in phase L2"
+            return "Number_of_voltage_swells_in_phase_L2"
         case '1-0:72.36.0':
-            return "Number of voltage swells in phase L3"
+            return "Number_of_voltage_swells_in_phase_L3"
         case '0-0:96.13.0':
-            return "Text message max 1024 characters"
+            return "Text_message_max_1024_characters"
         case '1-0:31.7.0':
-            return "Instantaneous current L1 in A resolution"
+            return "Instantaneous_current_L1_in_A_resolution"
         case '1-0:51.7.0':
-            return "Instantaneous current L2 in A resolution"
+            return "Instantaneous_current_L2_in_A_resolution"
         case '1-0:71.7.0':
-            return "Instantaneous current L3 in A resolution"
+            return "Instantaneous_current_L3_in_A_resolution"
         case '1-0:21.7.0':
-            return "Instantaneous active power L1 (+P) in W resolution"
+            return "Instantaneous_active_power_L1_(+P)_in_W_resolution"
         case '1-0:41.7.0':
-            return "Instantaneous active power L2 (+P) in W resolution"
+            return "Instantaneous_active_power_L2_(+P)_in_W_resolution"
         case '1-0:61.7.0':
-            return "Instantaneous active power L3 (+P) in W resolution"
+            return "Instantaneous_active_power_L3_(+P)_in_W_resolution"
         case '1-0:22.7.0':
-            return "Instantaneous active power L1 (-P) in W resolution"
+            return "Instantaneous_active_power_L1_(-P)_in_W_resolution"
         case '1-0:42.7.0':
-            return "Instantaneous active power L2 (-P) in W resolution"
+            return "Instantaneous_active_power_L2_(-P)_in_W_resolution"
         case '1-0:62.7.0':
-            return "Instantaneous active power L3 (-P) in W resolution"
+            return "Instantaneous_active_power_L3_(-P)_in_W_resolution"
         case '0-1:24.1.0':
             return "Device-Type"
         case '0-1:96.1.0':
-            return "Equipment id (Water)"
+            return "Equipment_id_(Water)"
         case '0-1:24.2.1':
-            return "Last 5-minute Meter reading in 0,001 m3 and capture time"
+            return "Last_5_minute_Meter_reading_in_0,001_m3_and_capture_time"
         default:
             return null
     }
